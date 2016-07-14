c01d457e19cda19d303f6b6546f952d27cb35c26
mredmundto <yatlunto@gmail.com>
2016-07-14 16:09:15 -0700

    added zeggio video player

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 var app = express(); 
 app.use(express.static(__dirname + '/../client'))
 app.port = 3000; 
+
 app.listen(app.port, function(){
 	console.log('we are listening!')
 }); 
