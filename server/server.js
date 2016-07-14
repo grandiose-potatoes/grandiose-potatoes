e8064d5e42a2cdaea5c15de51ca39c81891c1599
mredmundto <yatlunto@gmail.com>
2016-07-14 15:19:30 -0700

    server setup

---
+++ server/server.js
@@ -0,0 +1,9 @@
+var express = require('express'); 
+var bodyParser = require('body-parser'); 
+
+var app = express(); 
+app.use(express.static(__dirname + '/../client'))
+app.port = 3000; 
+app.listen(app.port, function(){
+	console.log('we are listening!')
+}); 
