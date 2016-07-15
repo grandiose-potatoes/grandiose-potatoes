758f465a76e53874eb513bbe0d53709ad1a69969
Billy Lan <billylan22@gmail.com>
2016-07-15 12:09:28 -0700

    Implemented a working component for video

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -4,15 +4,26 @@ import {VideoPlayer} from './VideoPlayer.jsx';
 export default class App extends React.Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			video: null
+		}
+	}
+
+	componentDidMount() {
+		//ajax request get back video object
+
+		this.setState({
+			video: {
+				url: 'https://www.youtube.com/embed/pckNsrbZU0U'
+			}
+		})
 	}
 
 	render() {
 		return (
 			<div>
 				<h2>yo</h2>
-				<VideoPlayer video={
-					{url: "supbreeeeh"}
-				}/>
+				<VideoPlayer video={this.state.video}/>
   		</div>
 		)
 	}
