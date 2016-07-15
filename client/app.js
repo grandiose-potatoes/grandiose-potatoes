b8d04fb1fffc589a8a94bd121887d35d5b4b8586
mredmundto <yatlunto@gmail.com>
2016-07-14 19:51:17 -0700

    Implement barebones for react

--- client/app.js
+++
@@ -1,1 +0,0 @@
-ReactDOM.render(<div>Hello bros</div>, document.getElementById('app'));
---
+++ client/components/app.jsx
@@ -0,0 +1,13 @@
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<div>
+				<h2>yo</h2>
+			</div>
+		)
+	}
+}
