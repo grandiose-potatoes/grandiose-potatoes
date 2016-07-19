b7eb050eeabe5c7e2b10ca78df129eb9260141cd
mofthecross <mofthecross@gmail.com>
2016-07-18 17:48:47 -0700

    Fix potential bugs

--- server/routes.js
+++ server/routes.js
@@ -1,4 +1,4 @@
-var controller = require('./controllers');
+var controller = require('./controllers.js');
 var router = require('express').Router();
 
 router.get('/api/questions', controller.questions.get);
--- server/server.js
+++ server/server.js
@@ -3,9 +3,9 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var router = require('./routes');
 
-
 var app = express();
-app.use(express.static(__dirname + '/../client'))
+
+app.use(express.static(__dirname + '/../client'));
 app.port = 3000;
 
 app.listen(app.port, function(){
@@ -17,25 +17,4 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //middleware for routes
 //https://expressjs.com/en/guide/routing.html
-app.use('/routes', router);
-
-// app.post('/api/questions', function(req, res) {
-//   console.log('req.body', req.body)
-//   //console.log('req.query', req.query);
-//
-//   var question = req.body.txt;
-//
-//   db.Question.create({txt: question})
-//   .then(function(question){
-//     console.log(question.get('txt'));
-//     res.end();
-//   })
-// });
-
-// app.get('/api/questions', function(req, res) {
-//   console.log('getting a request', req);
-//   db.Question.findAll().then(function(questions) {
-//     console.log('inside of server', questions);
-//     res.send(questions);
-//   });
-// });
+app.use('/', router);
