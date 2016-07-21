9a14faa93af522cbf5f31c2cbf1b9e6fe8f41005
Billy Lan <billylan22@gmail.com>
2016-07-21 01:31:48 -0700

    change app to lowercase a

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Router, Route} from 'react-router';
-import App from './components/App.jsx';
+import App from './components/app.jsx';
 import Record from './components/Record.jsx';
 import VideoPlayer from './components/VideoPlayer.jsx';
 
