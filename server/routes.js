af693d8f49ab09909c0f6d75f0a11d3a2e1ecc8f
Billy Lan <billylan22@gmail.com>
2016-07-20 23:08:04 -0700

    only send index for video and record endpoints

--- server/routes.js
+++ server/routes.js
@@ -8,6 +8,11 @@ router.get('/api/presigned', controller.videos.presigned)
 router.get('/api/videos', controller.videos.get);
 router.post('/api/videos', controller.videos.post);
 
-router.get('*', controller.home.get);
+router.get('/videos/*', controller.home.get);
+router.get('/record', controller.home.get);
+
+//TODO
+//Handle unknown routes;
+//router.get(*, errorHandler);
 
 module.exports = router;
