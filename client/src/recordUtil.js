571dfe06a61e2b6376e9f4216ff6253457402344
Clay Han <clayhan1@gmail.com>
2016-07-23 13:19:23 -0700

    Refactored one function into Utils. Cleaned up componentDidMount

--- client/src/recordUtil.js
+++ client/src/recordUtil.js
@@ -29,3 +29,18 @@ export let getSupportedTypes = () => {
   }
   return options;
 };
+
+
+//function that does a GET request to get an array of questions from our database
+export let getQuestions = (callback) => {
+  $.ajax({
+    type: 'GET', 
+    url: '/api/questions', 
+    success: function(data) {
+      callback(data);
+    },
+    error: function() {
+      console.log('Error with getQuestions');
+    }
+  });
+};
