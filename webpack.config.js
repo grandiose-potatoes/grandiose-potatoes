cc6ea6bc018850a6187610538904d838d4a1e7e7
mredmundto <yatlunto@gmail.com>
2016-07-24 10:21:12 -0700

    adding bundle in heroku

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   module: {
     loaders: [{
       test: /\.jsx?$/,
-      loader: 'babel',
+      loader: 'babel-loader',
       exclude: /node_modules/,
       query: {
         presets: ['react', 'es2015']
