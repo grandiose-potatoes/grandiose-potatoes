80443fbacc7e89ebe413b4a75b0fd71ed21f50f2
Billy Lan <billylan22@gmail.com>
2016-07-23 17:14:19 -0700

    refactor videoPlayer component

--- client/src/components/VideoPlayer.jsx
+++ client/src/components/VideoPlayer.jsx
@@ -9,37 +9,43 @@ export default class VideoPlayer extends React.Component {
 
   componentDidMount() {
     console.log('the params:', this.props.params.id);
-    this.getVideo(this.props.params.id);
-
+    this.setVideo(this.props.params.id);
   }
 
   render() {
     return (
-      <div>
+      <div className='col s8 offset-s2'>
         <h1>Video</h1>
-        <video controls src={this.state.videoUrl}/>
+        <video controls src={this.state.videoUrl} width="100%"/>
       </div>  
     );
   }
 
-  getVideo(code) {
-    let setVideoData = (data) => {
+  setVideo(code) {
+    this.getVideo(code)
+    .then((data) => {
       this.setState({
         videoUrl: data.url
       });
-    };
-    console.log('the code is:', code);
-    $.ajax({
-      type: 'GET',
-      url: '/api/videos',
-      data: {code: code},
-      success: function(data) {
-        console.log('the data is:', data);
-        setVideoData(data);
-      },
-      error: function(err) {
-        console.log('error getting video:', err);
-      }
-    });
+    })
+    .catch((err) => {
+      throw err;
+    })
+  }
+  getVideo(code) {
+    return new Promise((resolve, reject) => {
+      $.ajax({
+        type: 'GET',
+        url: '/api/videos',
+        data: {code: code},
+        success: function(data) {
+          console.log('the data is:', data);
+          resolve(data);
+        },
+        error: function(err) {
+          reject('error getting video:', err);
+        }
+      })
+    })
   }
 }
