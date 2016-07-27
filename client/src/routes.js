275a8c2af04f28f5db62df25bfa52bcbcb336c99
Robb Marsee <robb.marsee@gamil.com>
2016-07-26 17:08:59 -0700

    add routing to login page, add login page

--- client/src/routes.js
+++ client/src/routes.js
@@ -2,10 +2,12 @@ import React from 'react';
 import {Router, Route} from 'react-router';
 import App from './components/app.jsx';
 import Record from './components/Record.jsx';
+import Login from './components/Login.jsx';
 import VideoPlayer from './components/VideoPlayer.jsx';
 
 export default (
   <Route path="/" component={App}>
+    <Route path="login" component={Login}/>
     <Route path="record" component={Record}/>
     <Route path="videos/:id" component={VideoPlayer}/>
   </Route>
