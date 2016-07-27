7299d521d579dfe3f874d2bd308b4d95df6f3fe5
rhanzawa <rhanzawa@gmail.com>
2016-07-26 20:31:06 -0700

    Clean up and add React prop validation to app.jsx

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
-import {Home} from './Home.jsx';
+import { Home } from './Home.jsx';
 
-export default class App extends React.Component {
-	constructor(props) {
-		super(props);
-	}
-
-	//Render nested routes if the user has navigated to a nested route endpoint, otherwise render the home component
-	render() {
-		if (this.props.children) {
-			return (
-				<div className="row">
-					{this.props.children}
-	  		</div>
-			);
-		} else {
-			return (
-				<div>
-				  <Home />
-				</div>
-			);
-		}	
-	}
+function App(props) {
+  console.log(props.children);
+  if (props.children) {
+    return (
+      <div className="row">
+        {props.children}
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <Home />
+      </div>
+    );
+  }
 }
+
+App.propTypes = {
+  children: React.PropTypes.element,
+};
+
+export default App;
