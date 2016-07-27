5dddef2310b528d80d4f2518be723eec9c98b750
rhanzawa <rhanzawa@gmail.com>
2016-07-26 20:22:42 -0700

    Clean up index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {browserHistory, Router, Route} from 'react-router';
+import { browserHistory, Router, Route } from 'react-router';
 import routes from './routes.js';
 
 ReactDOM.render(
-  <Router history={browserHistory} routes={routes}>
-  </Router>
-  , document.getElementById('app')
+  <Router history={browserHistory} routes={routes} />,
+  document.getElementById('app')
 );
