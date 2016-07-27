b7d458114c1868a846424fced31754b41913d4fa
rhanzawa <rhanzawa@gmail.com>
2016-07-26 19:21:23 -0700

    Clean up homeController.js again

--- server/controllers/homeController.js
+++ server/controllers/homeController.js
@@ -1,8 +1,8 @@
 const path = require('path');
 
-const sendHome = (req, res) => {
+function sendHome(req, res) {
   res.sendFile(path.resolve(`${__dirname}/../../client/index.html`));
-};
+}
 
 module.exports = {
   sendHome,
