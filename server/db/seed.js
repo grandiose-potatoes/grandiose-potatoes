b492bb0e1a08ffe0297ba5f0f77dc583213913c1
rhanzawa <rhanzawa@gmail.com>
2016-07-26 19:54:14 -0700

    Clean up and fix typo in seed.js

--- server/db/seed.js
+++ server/db/seed.js
@@ -1,18 +1,17 @@
-var db = require('./db.js');
+const db = require('./db.js');
 
-var seedQuestuions = [
-	'What would constitute a “perfect” day for you?',
-	'Would you like to be famous? In what way?', 
-	'If you could change anything about the way you were raised what would it be?',
-	'When did you last cry in front of another person? By yourself?',
-	'If you die this evening without the opportunity to communicate with anyone, what would you most regret not having told someone? Why haven’t you told them yet?',
-	'What is the greatest accomplishment of your life?',
-	'What is your most terrible memory?',
-	'What, if anything, is too serious to be joked about?',
-	'Share an embarrassing moment in your life.'
-]; 
+const seedQuestions = [
+  'What would constitute a “perfect” day for you?',
+  'Would you like to be famous? In what way?',
+  'If you could change anything about the way you were raised what would it be?',
+  'When did you last cry in front of another person? By yourself?',
+  'If you die this evening without the opportunity to communicate with anyone, what would you most regret not having told someone? Why haven’t you told them yet?',
+  'What is the greatest accomplishment of your life?',
+  'What is your most terrible memory?',
+  'What, if anything, is too serious to be joked about?',
+  'Share an embarrassing moment in your life.',
+];
 
-seedQuestuions.forEach(question => {
-	db.Question.create({txt: question})	
+seedQuestions.forEach(question => {
+  db.Question.create({ txt: question });
 });
-
